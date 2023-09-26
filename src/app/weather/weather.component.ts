d60e0cf7a95f1bf379b3a68b409be87e97fc97f1
Huda-Alkhatri <hudalkhatri@outlook.com>
2023-09-26 17:31:36 +0400

    weather

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { WeatherService } from '../weather.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent {
+  weatherData: any;
+  error: string = '';
+  city: string = ''; // Define the city variable here
+
+  constructor(private weatherService: WeatherService) {}
+
+  searchWeather() { // Remove the city parameter from the function
+    this.weatherService.getWeatherData(this.city).subscribe(
+      (data) => {
+        this.weatherData = data;
+        this.error = ''; // Clear any previous error
+      },
+      (error) => {
+        this.weatherData = null;
+        this.error = 'City not found'; // Display error message
+      }
+    );
+  }
+
+}
